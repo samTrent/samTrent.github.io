9f2fcf611442bd1845e72954272afd690f0ec53e
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-03-19 14:12:31 -0600

    first commit

---
+++ engine.js
@@ -0,0 +1,39 @@
+console.log("TEST!");
+
+var dataURL = "http://samshomepage.dx.am/myWalmartWebpage/data.txt";
+
+//AJAX req
+function ajaxReq()
+{
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function()
+  {
+    //check to see if there was a success
+    if(this.readyState == 4 && this.status == 200)
+    {
+      //load the data into the html section
+      document.getElementById('ajaxSection').innerHTML = this.responseText;
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open("GET", dataURL, true);
+  xhttp.send(null);
+}
+
+
+function ajaxReqJSON()
+{
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function()
+  {
+    //check to see if there was a success
+    if(this.readyState == 4 && this.status == 200)
+    {
+      //load the data into the html section
+      document.getElementById('ajaxSection').innerHTML = this.responseText;
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open("GET", 'https://api.walmartlabs.com/v1/items/12417832?format=json&apiKey=uqtgph5ch964jknmyhbrxrxj', true);
+  xhttp.send();
+}
